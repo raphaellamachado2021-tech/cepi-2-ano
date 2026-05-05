a928cb2df3af7f67e0ca176985e789dc3929dcf9
raphaellamachado2021-tech <raphaellamachado2021@gmail.com>
2026-05-05 11:13:22 -0300

    Update retangulo.cs

--- projects/retangulo.cs
+++ projects/retangulo.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produtoEstoque
+{
+    internal class retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double Area()
+        {
+        return largura  * altura;
+        }
+        public double  Perimetro()
+        {
+
+            return 2*(largura + altura);
+        }
+        public double diagonal ()
+        {
+            return Math.Sqrt (largura* largura + altura* altura);
+        }
+    }
+}
 
