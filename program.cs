0bf9b4213b0cd00e33ffb2d31ca3aa3c12b993fa
raphaellamachado2021-tech <raphaellamachado2021@gmail.com>
2026-05-05 10:31:09 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,1 +1,33 @@
+using produtoEstoque;
+using System;
+using System.Globalization;
+
+namespace exercicio_funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine("");
+            Console.Write("Deseja aumentar o salário em qual porcentagem: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
 
