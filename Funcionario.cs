71d6a931e4f1cfd5b7c129450ad64f409c1ff11f
raphaellamachado2021-tech <raphaellamachado2021@gmail.com>
2026-05-05 10:24:54 -0300

    Update Funcionario.cs

--- Funcionario.cs
+++ Funcionario.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produtoEstoque
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.00);
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", R$"
+            + SalarioLiquido();
+
+
+        }
+    }
+}
 
